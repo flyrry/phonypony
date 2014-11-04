9ccfa0b87f461e6cb247992b2e43594141c6c93b
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-11-04 22:07:07 +0200

    Cleanup output of console runner. Print view url

--- src/Vindinium/Runner/ConsoleVindinium.hs
+++ src/Vindinium/Runner/ConsoleVindinium.hs
@@ -6,6 +6,8 @@ import Data.Aeson
 import Control.Applicative (Applicative)
 import Control.Monad.Reader (ReaderT, ask, runReaderT)
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 
 import Vindinium.Types
 import Vindinium.Api
@@ -18,18 +20,19 @@ instance Vindinium ConsoleVindinium where
     move s d = ConsoleVindinium $ do
       settings <- ask
       newState <- liftIO $ request settings (statePlayUrl s) (object [("dir", toJSON d)])
-      liftIO $ print (show newState)
       return newState
     playTraining mt mb b = (ConsoleVindinium $ do
       settings <- ask
       initialState <- liftIO (startTraining settings mt mb)
-      -- TODO: show initial state in console (if needed)
+      liftIO $ TIO.putStrLn $ T.append "View url: " (stateViewUrl initialState)
       return initialState) >>= playLoop b
     playArena b = (ConsoleVindinium $ do
       settings <- ask
       initialState <- liftIO (startArena settings)
-      -- TODO: show initial state in console (if needed)
+      liftIO $ TIO.putStrLn "Got initial state from the server"
+      liftIO $ TIO.putStrLn $ T.append "View url: " (stateViewUrl initialState)
       return initialState) >>= playLoop b
+    outputState = undefined -- TODO: if needed
 
 playLoop :: Vindinium m => Bot -> State -> m State
 playLoop bot state =
